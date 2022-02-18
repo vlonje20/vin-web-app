e85afa4c38335b200a62ea6b494a7d0867c6c905
Esso4real <81115661+Esso4real@users.noreply.github.com>
2022-02-17 20:31:50 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-DOCKER_IMAGE=esso4real/pipeline:v28
+DOCKER_IMAGE=esso4real/pipeline:v29
 docker pull $DOCKER_IMAGE
-docker run -d -p 8081:8081 $DOCKER_IMAGE
+docker run -d -p 8085:8085 $DOCKER_IMAGE
